f6fba5a2ffbab76e0ee50e4a8d5acdc4b02e5d18
sandeshghimire2020 <152824228+prakriti1290@users.noreply.github.com>
2023-12-05 22:32:57 -0600

    enhancing JS file

--- script.js
+++ script.js
@@ -1,58 +1,54 @@
-document.getElementById("breakfastButton").addEventListener("click", function() {
-    fetch("/meals/breakfast")
-        .then(response => {
+document.getElementById("breakfastButton").addEventListener("click", function () {
+    fetchMenu("breakfast");
+});
+
+document.getElementById("dinnerButton").addEventListener("click", function () {
+    fetchMenu("dinner");
+});
+
+function fetchMenu(type) {
+    fetch(`https://menu-app-jcn0.onrender.com/meals/${type}`)
+        .then((response) => {
             if (!response.ok) {
                 throw new Error("Network response was not ok");
             }
             return response.json();
         })
-        .then(data => {
+        .then((data) => {
             // Update the #mealDetails div with fetched meal details
             displayMealDetails(data);
         })
-        .catch(error => {
-            console.error("Error fetching breakfast meal data:", error);
+        .catch((error) => {
+            console.error(`Error fetching ${type} meal data:`, error);
             // Handle the error as needed, e.g., display an error message
         });
-});
+}
 
-document.getElementById("dinnerButton").addEventListener("click", function() {
-    fetch("/meals/dinner")
-        .then(response => {
-            if (!response.ok) {
-                throw new Error("Network response was not ok");
-            }
-            return response.json();
-        })
-        .then(data => {
-            // Update the #mealDetails div with fetched meal details
-            displayMealDetails(data);
-        })
-        .catch(error => {
-            console.error("Error fetching dinner meal data:", error);
-            // Handle the error as needed, e.g., display an error message
-        });
-});
-
-function displayMealDetails(meal) {
+function displayMealDetails(meals) {
     const mealDetailsContainer = document.getElementById("mealDetails");
 
     // Clear any existing content in the mealDetailsContainer
     mealDetailsContainer.innerHTML = "";
 
-    // Create HTML elements to display meal details
-    const mealName = document.createElement("h2");
-    mealName.textContent = meal.name;
+    // Loop through each meal and create HTML elements to display meal details
+    meals.forEach((meal) => {
+        const mealItem = document.createElement("div");
+        mealItem.classList.add("meal-item");
 
-    const mealDescription = document.createElement("p");
-    mealDescription.textContent = meal.description;
+        const mealName = document.createElement("h2");
+        mealName.textContent = meal.name;
 
-    const mealImage = document.createElement("img");
-    mealImage.src = meal.imageUrl;
-    mealImage.alt = meal.name;
+        const mealDescription = document.createElement("p");
+        mealDescription.textContent = meal.description;
 
-    // Append the elements to the mealDetailsContainer
-    mealDetailsContainer.appendChild(mealName);
-    mealDetailsContainer.appendChild(mealDescription);
-    mealDetailsContainer.appendChild(mealImage);
+        const mealImage = document.createElement("img");
+        mealImage.src = meal.imageUrl;
+        mealImage.alt = meal.name;
+
+        mealItem.appendChild(mealName);
+        mealItem.appendChild(mealDescription);
+        mealItem.appendChild(mealImage);
+
+        mealDetailsContainer.appendChild(mealItem);
+    });
 }
